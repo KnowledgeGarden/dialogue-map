25ede5a593b813d98e6c40899a3ad82b7ccac3e0
Jack Park <jackpark@gmail.com>
2017-08-28 19:05:38 -0700

    first commit

---
+++ src/server/backside/AuthDriver.js
@@ -0,0 +1,58 @@
+import BacksideDriver from './BacksideDriver';
+import config from './backside.config';
+import {
+  StatusCodeError,
+  ResponseMessageError
+} from './BacksideErrors';
+
+export default class AuthDriver extends BacksideDriver {
+  // Methods correspond with verbs in src/main/java/org/topicquests/backside/servlet/apps/auth/api/IAuthMicroformat.java
+
+  // TODO(wenzowski): is this the right way to use the 'Auth' verb?
+  async login(email, password, query = {}) {
+    const endpoint = this.basicAuthEndpoint(email, password, query);
+    const body = await this.get(endpoint);
+    if (typeof body.rMsg !== 'string') throw new ResponseMessageError(body.rMsg);
+    if (!body.cargo) return [body.rToken, {}];
+    const {uEmail, uName, uFullName, uRole} = body.cargo;
+    return [
+      body.rToken,
+      {
+        email: uEmail,
+        handle: uName,
+        name: uFullName,
+        role: (uRole || '').split(',').map(str => str.trim()).filter(str => str)
+      }
+    ];
+  }
+
+  // TODO(wenzowski): fix BacksideServlet so logging out a user twice does not result in a 500 response
+  async logout(sToken, query = {}) {
+    const endpoint = this.endpoint('/auth/', {verb: 'LogOut', sToken, ...query});
+    const body = await this.get(endpoint).catch(error => {
+      if (!error instanceof StatusCodeError) throw error;
+    });
+    if (body && body.rMsg === 'ok') return true;
+    return false;
+  }
+
+  async isHandleAvailable(uName) {
+    if (!uName) throw new ReferenceError('Missing required argument uName');
+    const endpoint = this.endpoint('/auth/', {verb: 'Validate', uName});
+    const body = await this.get(endpoint);
+    if (body.rMsg === 'not found') return false;
+    if (body.rMsg === 'ok') return true;
+    throw new ResponseMessageError(body.rMsg);
+  }
+
+  async isEmailAvailable(uEmail) {
+    if (!uEmail) throw new ReferenceError('Missing required argument uEmail');
+    const endpoint = this.endpoint('/auth/', {verb: 'ExstEmail', uEmail});
+    const body = await this.get(endpoint);
+    if (body.rMsg === 'not found') return true;
+    if (body.rMsg === 'ok') return false;
+    throw new ResponseMessageError(body.rMsg);
+  }
+}
+
+export const auths = new AuthDriver(config);
---
+++ src/server/graphql/models/Posts/postMutation.js
@@ -0,0 +1,28 @@
+import {GraphQLNonNull} from 'graphql';
+import {topicMap} from '../../../backside/TopicMapDriver';
+// import {errorObj} from '../utils';
+import {isLoggedIn} from '../authorization';
+import {Post, NewPost} from './postSchema';
+
+export default {
+  createPost: {
+    type: Post,
+    args: {
+      post: {type: new GraphQLNonNull(NewPost)}
+    },
+    async resolve(source, {post}, authToken) {
+      isLoggedIn(authToken);
+      const {token, handle} = authToken;
+      const {language, title, details, icon, isPrivate} = post;
+      const newPost = await topicMap.addTopic({token, handle}, {
+        language: (language || 'en'),
+        label: title,
+        details,
+        icon: (icon || 'publication'),
+        isPrivate: (isPrivate || false),
+        instanceOf: 'BlogNodeType'
+      });
+      return newPost;
+    }
+  }
+};
